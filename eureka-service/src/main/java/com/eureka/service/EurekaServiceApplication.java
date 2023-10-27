de6c97529d049547580fb57a3d4146fea7c1fb6e
Harlam7 <titotomaswashere@gmail.com>
2023-10-27 11:55:55 -0500

    commit para el server eureka

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -1,21 +1,15 @@
-package com.usuario.service;
+package com.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
-@EnableFeignClients
 @SpringBootApplication
 @EnableEurekaServer
-public class UsuarioServiceApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(UsuarioServiceApplication.class, args);
-		System.out.println("probando probando 1 2 3");
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
